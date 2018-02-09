47dc593787aa07c1b96a2fdb325d80e37e0f9a68
Cloud User <centos@node-0.example.com>
2018-02-09 02:05:39 +0000

    final push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
-sudo openssl s_client -connect ${DTR_IPADDR}:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee /usr/local/share/ca-certificates/${DTR_IPADDR}.crt
+sudo openssl s_client -connect ${DTR_IPADDR}:4443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee /usr/local/share/ca-certificates/${DTR_IPADDR}.crt
 sudo update-ca-certificates
 /bin/tini -- /usr/local/bin/jenkins.sh
-###
